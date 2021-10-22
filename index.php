0d707c1f5a731bf1de85ca58a2210c44c94bbbc5
cskiller24 <ccadayong2020@plm.edu.ph>
2021-10-22 23:48:56 +0800

    Moved Files

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require("dbconnection.php");
-
-    if (!$conn) {
-        echo "Failed to connect to MySQL Database:" . mysqli_connect_error();
-        die();
-    }
-    else header("Location: view/buyer/homepage.php");
--- logout_seller.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-if (isset($_SESSION["seller_id"])) {
-  unset($_SESSION["seller_id"]);
-  session_unset();
-}
-header("Location: login_seller.php");
-die();
