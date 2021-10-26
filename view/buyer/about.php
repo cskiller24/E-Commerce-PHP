d768763a9c7a23c7c8749ff39bb4491999a3dc14
cskiller24 <ccadayong2020@plm.edu.ph>
2021-10-26 10:10:51 +0800

    added about page

---
+++ view/buyer/about.php
@@ -0,0 +1,27 @@
+<?php session_start()?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="style/nav.css">
+    <link rel="stylesheet" href="style/about.css">
+    <title>Demo Store | About</title>
+</head>
+<body>
+    <?php include("header.php"); ?>
+
+    <div class="background">
+        <div class="container text-light p-5 mt-5">
+            <h1>ABOUT</h1>
+
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt, eum quod odit veniam voluptatem quibusdam laboriosam in velit ratione excepturi minus deleniti ipsum dolores. Incidunt dignissimos explicabo recusandae laborum facilis odio quam! Quidem nesciunt a quaerat architecto. Dolore veniam aperiam accusantium deleniti, nulla laboriosam sit provident dolor quod odio! Culpa illo autem odit. Similique laboriosam cupiditate officiis voluptates inventore fugit necessitatibus nulla expedita earum pariatur soluta fugiat corrupti, quis iure tempora repellendus exercitationem laborum iusto distinctio. Consequuntur eius modi neque! Fugiat eum molestias nostrum numquam non veritatis quam eaque ab, eos sapiente et reprehenderit sed reiciendis dolore quis omnis quas.</p>
+        </div>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>
