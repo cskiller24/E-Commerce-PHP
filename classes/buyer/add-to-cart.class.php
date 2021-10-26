9c6a60b59aab110d8082ec4d8a6bc72a915aa0cb
cskiller24 <ccadayong2020@plm.edu.ph>
2021-10-26 11:27:05 +0800

    added separate add to cart function

---
+++ classes/buyer/add-to-cart.class.php
@@ -0,0 +1,43 @@
+<?php 
+    if(isset($_POST['add-to-cart-view'])){
+        $product_id = $_POST['product_id'];
+        $seller_id = $_POST['seller_id'];
+        $buyer_id = $_POST['buyer_id'];
+
+        include("../../dbconnection.php");
+        include("../../functions/buyer/add-to-cart.fx.php");
+
+        $result = addToCart($conn, $product_id, $buyer_id, $seller_id);
+
+        if($result){
+            header("Location: ../../view/buyer/view.php?pid=$product_id&cart=success");
+            exit();
+        }
+        else{
+            header("Location: ../../view/buyer/view.php?pid=product_id&cart=failed");
+            exit();
+        }
+    }
+
+    if(isset($_POST['add-to-cart-homepage'])){
+        $product_id = $_POST['product_id'];
+        $seller_id = $_POST['seller_id'];
+        $buyer_id = $_POST['buyer_id'];
+        $page = $_POST['page'];
+
+        include("../../dbconnection.php");
+        include("../../functions/buyer/add-to-cart.fx.php");
+
+        $result = addToCart($conn, $buyer_id, $product_id, $seller_id);
+
+        
+        if($result){
+            header("Location: ../../view/buyer/homepage.php?page=$page&cart=success");
+            exit();
+        }
+        else{
+            header("Location: ../../view/buyer/homepage.php?pid=$page&cart=failed");
+            exit();
+        }
+    }
+?>
