0f4d2a44ae73276a0995378174be457a9bbe3f4e
cskiller24 <ccadayong2020@plm.edu.ph>
2021-10-30 20:34:02 +0800

    fix transaction delete

--- classes/buyer/transaction.class.php
+++ classes/buyer/transaction.class.php
@@ -9,8 +9,18 @@
         for($i = 0; $i < $result_count; $i++){
             $seller = getSellerDetails($conn, $result[$i]['seller_id']);
             $product = getProductDetails($conn, $result[$i]['product_id']);
-            $result[$i]['seller_name'] = $seller['seller_name'];
-            $result[$i]['product_name'] = $product['product_name'];
+            if($seller == false){
+                $result[$i]['seller_name'] = "Deleted";
+            }
+            else{
+                $result[$i]['seller_name'] = $seller['seller_name'];
+            }
+            if($product == false){
+                $result[$i]['product_name'] = "Deleted";
+            }
+            else{
+                $result[$i]['product_name'] = $product['product_name'];
+            }
         }
     }
 
--- classes/seller/transaction.class.php
+++ classes/seller/transaction.class.php
@@ -10,6 +10,16 @@
     for($i = 0; $i < $result_count; $i++){
         $buyer_result = getBuyerDetails($conn, $result[$i]['buyer_id']);
         $product_result = getProductDetails($conn, $result[$i]['product_id']);
-        $result[$i]['buyer_name'] = $buyer_result['buyer_name'];
-        $result[$i]['product_name'] = $product_result['product_name']; 
+        if($buyer_result == false){
+            $result[$i]['buyer_name'] = $buyer_result['buyer_name'];
+        }
+        else{
+            $result[$i]['buyer_name'] = $buyer_result['buyer_name'];
+        }
+        if($product_result == false){
+            $result[$i]['product_name'] = "Deleted"; 
+        }
+        else{
+            $result[$i]['product_name'] = "Deleted"; 
+        }
     }
